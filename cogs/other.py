b3f68762f4cf8670f2fac5ac89f888d9dc026965
sparrow001 <66333207+sparrow001@users.noreply.github.com>
2021-12-28 11:10:09 -0500

    Adds the Suggestion Command

--- cogs/other.py
+++ cogs/other.py
@@ -1,6 +1,9 @@
 import DPyUtils
 import json
+import discord
 from discord.ext import commands
+from discord.ext.commands.core import Command
+from discord.ext.commands.errors import CommandOnCooldown
 
 
 class Other(commands.Cog):
@@ -50,6 +53,42 @@ class Other(commands.Cog):
                 description=f"[Click here to invite the bot to your server.]({discord.utils.oauth_url(self.bot.user.id, scopes=('applications.commands', 'bot'), permissions=discord.Permissions(412317240384))})",
             )
         )
+
+    @commands.command(name="suggestq")
+    @commands.cooldown(1,60,commands.BucketType.user)
+    async def suggestq(self, ctx: DPyUtils.Context, question: str):
+        """
+        Suggest a question.
+        """
+        try: 
+            c = self.bot.get_channel(925420070774124565)
+            embed = self.bot.Embed(
+                title="Suggestion Created",
+                description=f"{ctx.author} suggested a quesiton")
+            embed.add_field(name="Question", value=question)
+            await c.send(embed=embed)
+            await ctx.send("Your Suggestion has been sent!")
+
+        except CommandOnCooldown:
+            await ctx.send(embed=self.bot.Embed(title="Cooldown", description="You are on cooldown. Please wait a minute before trying again."))
+
+        except Exception as e:
+            if isinstance(e, CommandOnCooldown):
+                pass
+            else:
+                error = self.bot.Embed(
+                    title=f"Error while running command ('{ctx.command}')",
+                    description=f"{e}",
+                    color = discord.Color.dark_red()
+                )
+                usererror = self.bot.Embed(
+                    title=f"Error while running command ('{ctx.command}')",
+                    description="Sorry, an error has occured while running this command. Try again in a few minutes, a traceback has been sent to the developers.",
+                    color = discord.Color.dark_red()
+                )
+                await c.send(embed=error)
+                await ctx.send(embed=usererror)
+        
 
 
 def setup(bot: DPyUtils.Bot):
